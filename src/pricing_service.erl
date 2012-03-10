645d6089d2cc7d66700aca12cb30641d531a31b8
Nick McDowall <nicholasinengland@hotmail.com>
2012-03-10 19:36:55 +0000

    added a version request type to check the version - useful when asking clients to check version to make sure that there are not two versions behind - which can cause hot swapping error.

--- src/pricing_service.erl
+++ src/pricing_service.erl
@@ -1,5 +1,6 @@
 -module(pricing_service).
 -export([register/1, message_receiver/0, handle/1]).
+-define(VERSION, '1.10').
 
 register(ClientPid) ->
 	register(client, ClientPid),
@@ -7,19 +8,26 @@ register(ClientPid) ->
 
 message_receiver() ->
 	receive
-		Request -> ?MODULE:handle(Request),
-		message_receiver()
+		Request 	-> ?MODULE:handle(Request)
+	end,
+	?MODULE:message_receiver().
+
+handle(Request) -> 
+	case Request of 
+		version 		-> reply_with_version();
+		{price, Item} 	-> reply_with_price_of(Item)
 	end.
 
-handle({price, Item}) -> reply_with_price_of(Item).	
+reply_with_version() ->
+	client ! io:format("Service Version:[~p]~n",[?VERSION]).
 
 reply_with_price_of(Item) ->
-	client ! io:format("The price of ~p is: $~p~n", [Item, price(Item)]).
+	client ! io:format("The price of ~p is: $~p ~n", [Item, price(Item)]).
 
 price(Item) -> 
 	case Item of
-		tea 	-> 2.39;
-		coffee 	-> 2.63;
+		tea 	-> 2.05;
+		coffee 	-> 2.10;
 		milk 	-> 0.97;
 		bread 	-> 0.50
 	end.
